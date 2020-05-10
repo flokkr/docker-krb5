103ebe3c7a9f0635ceed542a92ab0b9fac823e70
Elek Márton <elek@apache.org>
2020-05-10 11:20:59 +0200

    remove bash scripts required by the issuer REST endpoint

--- bash/keytab.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-keytab(){
-  echo "Generiting keytab"
-  kadmin -kt /tmp/admin.keytab -p admin/admin -q "addprinc -randkey $1"
-  kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k $2 $1"
-}
